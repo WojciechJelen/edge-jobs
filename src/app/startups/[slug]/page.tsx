fe8ecf0487a12e3f8ca016fc2c9e96a68dfc708e
Wojciech Jelen <w.jelen20@gmail.com>
2023-05-22 16:49:42 +0200

    to run the build on vercel

--- src/app/startups/[slug]/page.tsx
+++ src/app/startups/[slug]/page.tsx
@@ -12,27 +12,28 @@ interface Props {
   params: { slug: string };
 }
 
-export async function generateStaticParams() {
-  const startups: StartupCompany[] = await fetch(
-    "http://localhost:3000/api/startups",
-    { cache: "force-cache" }
-  ).then((res) => res.json());
+// export async function generateStaticParams() {
+//   const startups: StartupCompany[] = await fetch(
+//     "http://localhost:3000/api/startups",
+//     { cache: "force-cache" }
+//   ).then((res) => res.json());
 
-  return startups.map((s) => ({ slug: s.slug }));
-}
+//   return startups.map((s) => ({ slug: s.slug }));
+// }
 
 export default async function StartupPage({ params }: Props) {
-  const startups: StartupCompany[] = await fetch(
-    "http://localhost:3000/api/startups",
-    { cache: "force-cache" }
-  ).then((res) => res.json());
+  // const startups: StartupCompany[] = await fetch(
+  //   "http://localhost:3000/api/startups",
+  //   { cache: "force-cache" }
+  // ).then((res) => res.json());
 
-  const startup = startups.find((s) => s.slug === params.slug)!;
+  // const startup = startups.find((s) => s.slug === params.slug)!;
 
   return (
     <div>
-      <h1>{startup.name}</h1>
-      <p>{startup.description}</p>
+      {/* <h1>{startup.name}</h1>
+      <p>{startup.description}</p> */}
+      <div>Startups TBD</div>
     </div>
   );
 }
